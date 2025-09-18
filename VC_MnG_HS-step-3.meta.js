a9ae5bf7a97839c47dee796390a9a66f94462653
Your Name <egov-99@gmail.com>
2025-09-18 12:50:38 +0800

    first commit

---
+++ VC_MnG_HS-step-3.meta.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name         VC/MnG/HS step [3] - Auto Submit VC/MnG/HS Konfirmasi
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Submit form otomatis ketika tombol filter ditekan
+// @match        https://jkt48.com/tickets/handshake/address/hid*
+// @grant        none
+// ==/UserScript==
---
+++ VC_MnG_HS-step-3.user.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         VC/MnG/HS step [3] - Auto Submit VC/MnG/HS Konfirmasi
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Submit form otomatis ketika tombol filter ditekan
+// @match        https://jkt48.com/tickets/handshake/address/hid*
+// @grant        none
+// @updateURL    https://raw.github.com/crimskid/supptools_jkt48_com/VC_MnG_HS-step-3.meta.js
+// @downloadURL  https://raw.github.com/crimskid/supptools_jkt48_com/VC_MnG_HS-step-3.user.js
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  function autoClick() {
+    const checkbox = document.querySelector("#agree");
+    if (checkbox) {
+      checkbox.checked = true;
+    }
+    // cari tombol submit berdasarkan teks dan class
+    const button = [
+      ...document.querySelectorAll("button.btn.btn-block.btn-pink"),
+    ].find((b) => b.textContent.trim() === "Konfirmasi");
+
+    if (button) {
+      button.click(); // klik otomatis
+      //console.log("Tombol Konfirmasi sudah diklik otomatis");
+    } else {
+      //console.log("Tombol Konfirmasi tidak ditemukan");
+    }
+  }
+
+  // Run once
+  autoClick();
+
+  // Keep watching for dynamically added posts
+  const observer = new MutationObserver(autoClick);
+  observer.observe(document.body, { childList: true, subtree: true });
+})();
