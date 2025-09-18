aa23cddae3e026731adac905968d219a3ab8e2b2
Your Name <egov-99@gmail.com>
2025-09-18 13:18:45 +0800

    process-update another-4

--- VC_MnG_HS-step-1.user.js
+++ VC_MnG_HS-step-1.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VC/MnG/HS step [1] - Hide El & SUbmit
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.2
 // @description  VC/MnG/HS - Hide El & SUbmit
 // @author       IceCrims
 // @match        https://jkt48.com/tickets/handshake?*
--- VC_MnG_HS-step-3.user.js
+++ VC_MnG_HS-step-3.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VC/MnG/HS step [3] - Auto Submit VC/MnG/HS Konfirmasi
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.2
 // @description  Submit form otomatis ketika tombol filter ditekan
 // @author       IceCrims
 // @match        https://jkt48.com/tickets/handshake/address/hid*
