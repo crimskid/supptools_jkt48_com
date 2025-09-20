e3cfbf9353ad5201482299fbf498e3dc7a256f66
Your Name <egov-99@gmail.com>
2025-09-20 19:59:44 +0800

    process-update added local store-2

--- VC_MnG_HS-step-2.meta.js
+++ VC_MnG_HS-step-2.meta.js
@@ -1,9 +1,11 @@
 // ==UserScript==
 // @name         VC/MnG/HS step [2] - Hide EL Except Selected
 // @namespace    http://tampermonkey.net/
-// @version      1.3
+// @version      1.4
 // @description  Hide all form-group elements except the one with Celline Thefani
 // @author       IceCrims
 // @match        https://jkt48.com/tickets/handshake/form/hid/*
 // @grant        GM_addStyle
+// @grant        GM_getValue
+// @grant        GM_setValue
 // ==/UserScript==
