dda1f72cdf660842fb68bb8f9acd579313bf88dd
Your Name <egov-99@gmail.com>
2025-09-20 20:51:23 +0800

    process-update added local store-7

--- VC_MnG_HS-step-2.user.js
+++ VC_MnG_HS-step-2.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VC/MnG/HS step [2] - Hide EL Except Selected
 // @namespace    http://tampermonkey.net/
-// @version      1.7
+// @version      1.8
 // @description  Hide all form-group elements except the one with Selected
 // @author       IceCrims
 // @match        https://jkt48.com/tickets/handshake/form/hid/*
