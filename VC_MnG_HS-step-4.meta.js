e0c9bbcbae1831fe32da883c0544920fa5ac8aff
Kang Listrik-v1 <pokemon.crims1412@gmail.com>
2025-11-08 14:01:12 +0800

    upd: update step-2. update step-3 for auto comp-3

--- VC_MnG_HS-step-4.meta.js
+++ VC_MnG_HS-step-4.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VC/MnG/HS step [4] - Auto execute/comp Konfirmasi
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Submit Akhir, berbahaya
 // @author       IceCrims
 // @match        https://jkt48.com/tickets/handshake/comp/hid*
